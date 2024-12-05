2f9ff777d466e4248dea4c141cb4d11493c19548
Akintunde Jegede <jegedeakintunde@gmail.com>
2024-12-05 15:31:21 +0100

    switched to http

--- src/logger.ts
+++ src/logger.ts
@@ -2,13 +2,12 @@ import os from "os";
 import winston from "winston";
 import "winston-syslog";
 
-const SYSTEM_TRANSPORT = new (winston.transports as any).Syslog({
+const SYSTEM_TRANSPORT = new winston.transports.Http({
 	host: process.env.host,
 	port: process.env.port ? parseInt(process.env.port, 10) : undefined,
-	localhost: process.env.hostname || os.hostname(),
-	app_name: process.env.module_name || "pm2-pp-logger",
-	protocol: "tls4",
-	eol: "\n",
+	// localhost: process.env.hostname || os.hostname(),
+	// app_name: process.env.module_name || "pm2-pp-logger",
+	ssl: false,
 });
 
 const LOGGER = winston.createLogger({
@@ -37,8 +36,8 @@ export class Logger {
 				return;
 			}
 
-			SYSTEM_TRANSPORT.localhost = this.name;
-			SYSTEM_TRANSPORT.appName = this.appName;
+			// SYSTEM_TRANSPORT.localhost = this.name;
+			// SYSTEM_TRANSPORT.appName = this.appName;
 
 			LOGGER.log(level, message);
 		} catch (error) {
