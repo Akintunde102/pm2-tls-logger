9547c2e6e56c78d5a7a01701d07f8c8e4424280d
Akintunde Jegede <jegedeakintunde@gmail.com>
2024-12-05 09:23:00 +0100

    remove js from source code

--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -45,13 +12,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const io = __importStar(require("@pm2/io"));
-const pm2 = __importStar(require("pm2"));
-const logger_1 = __importDefault(require("./logger"));
-const lib_1 = require("./lib");
-const METRICS = {
-    processes: io.metric({ name: "Attached Processes", value: () => 0 })
-};
+const io_1 = __importDefault(require("@pm2/io"));
+const pm2_1 = __importDefault(require("pm2"));
+const logger_1 = require("./src/logger");
+const lib_1 = require("./src/lib");
+const METRICS = io_1.default.metric({
+    name: "Attached Processes"
+});
 const TRANSPORTS = {};
 let PM2_CONFIG = {};
 function routeLog(packet, level = "info") {
@@ -61,25 +28,25 @@ function routeLog(packet, level = "info") {
     if (!TRANSPORTS[processName]) {
         const systemName = PM2_CONFIG.hostname;
         const appName = processName;
-        TRANSPORTS[processName] = new logger_1.default(systemName, appName);
-        METRICS.processes.set(Object.keys(TRANSPORTS).length);
+        TRANSPORTS[processName] = new logger_1.Logger(systemName, appName);
+        METRICS.set(Object.keys(TRANSPORTS).length);
         console.log(`Created new logger for process: ${processName}`);
     }
     return TRANSPORTS[processName].log(level, packet.data);
 }
-io.init().initModule(false, (error) => {
+io_1.default.init().initModule(false, (error) => {
     const isInitError = (0, lib_1.handleInitError)(error);
     if (isInitError)
         return;
-    const { configIsValid, moduleConfig } = (0, lib_1.handleModuleConfig)(io);
+    const { configIsValid, moduleConfig } = (0, lib_1.handleModuleConfig)(io_1.default);
     if (!configIsValid)
         return;
     PM2_CONFIG = moduleConfig;
     PM2_CONFIG.hostname = (0, lib_1.getHostName)(moduleConfig);
-    pm2.connect(() => {
+    pm2_1.default.connect(() => {
         console.log(`
             Started and forwarding log outputs to ${PM2_CONFIG.host}:${PM2_CONFIG.port} as ${`system name '${PM2_CONFIG.hostname}'`}.`);
-        pm2.launchBus((busError, bus) => {
+        pm2_1.default.launchBus((busError, bus) => {
             if (busError) {
                 console.error("Error launching pm2 bus:", busError);
                 return;
@@ -89,14 +56,14 @@ io.init().initModule(false, (error) => {
             bus.on("process:event", (packet) => {
                 if (packet.event === "exit" && TRANSPORTS[packet.process.name]) {
                     delete TRANSPORTS[packet.process.name];
-                    METRICS.processes.set(Object.keys(TRANSPORTS).length);
+                    METRICS.set(Object.keys(TRANSPORTS).length);
                     console.log(`Closed transport pipeline for exiting process '${packet.process.name}'.`);
                 }
             });
         });
     });
 });
-io.action("show-attached-processes", (callback) => __awaiter(void 0, void 0, void 0, function* () {
+io_1.default.action("show-attached-processes", (callback) => __awaiter(void 0, void 0, void 0, function* () {
     const total = Object.keys(TRANSPORTS).length;
     if (total === 0) {
         console.log("[Attached Processes] Found no attached processes, a process will be attached when it logs for the first time.");
--- src/logger.d.ts
+++
@@ -1,8 +0,0 @@
-import "winston-syslog";
-export declare class Logger {
-    private name;
-    private appName;
-    private process;
-    constructor(systemName: string, programName: string);
-    log(level: string, message: string): void;
-}
