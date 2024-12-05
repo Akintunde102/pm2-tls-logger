56f34ce66f84cb2fcfe74487ba38a6e04ad2b46f
Akintunde Jegede <jegedeakintunde@gmail.com>
2024-12-05 15:31:51 +0100

    add js build file

--- logger.js
+++ logger.js
@@ -4,16 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Logger = void 0;
-const os_1 = __importDefault(require("os"));
 const winston_1 = __importDefault(require("winston"));
 require("winston-syslog");
-const SYSTEM_TRANSPORT = new winston_1.default.transports.Syslog({
+const SYSTEM_TRANSPORT = new winston_1.default.transports.Http({
     host: process.env.host,
     port: process.env.port ? parseInt(process.env.port, 10) : undefined,
-    localhost: process.env.hostname || os_1.default.hostname(),
-    app_name: process.env.module_name || "pm2-pp-logger",
-    protocol: "tls4",
-    eol: "\n",
+    // localhost: process.env.hostname || os.hostname(),
+    // app_name: process.env.module_name || "pm2-pp-logger",
+    ssl: false,
 });
 const LOGGER = winston_1.default.createLogger({
     format: winston_1.default.format.printf(({ message }) => message),
@@ -32,8 +30,8 @@ class Logger {
                 console.error(`Discarding log from '${this.process}' astransport is not ready.`);
                 return;
             }
-            SYSTEM_TRANSPORT.localhost = this.name;
-            SYSTEM_TRANSPORT.appName = this.appName;
+            // SYSTEM_TRANSPORT.localhost = this.name;
+            // SYSTEM_TRANSPORT.appName = this.appName;
             LOGGER.log(level, message);
         }
         catch (error) {
